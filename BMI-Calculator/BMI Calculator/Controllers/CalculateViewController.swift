06d671f4a0a5f8df4dd53ab4436c055c519704f8
Münevver Elif Ay <105984573+munevverelifay@users.noreply.github.com>
2023-02-21 21:07:06 +0300

    Add files via upload

---
+++ BMI-Calculator/BMI Calculator/Controllers/CalculateViewController.swift
@@ -0,0 +1,55 @@
+//
+//  ViewController.swift
+//  BMI Calculator
+//
+//  Created by Angela Yu on 21/08/2019.
+//  Copyright © 2019 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class CalculateViewController: UIViewController {
+
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var weightSlider: UISlider!
+    @IBOutlet weak var currentHeightLabel: UILabel!
+    @IBOutlet weak var calculateButton: UIButton!
+    @IBOutlet weak var currentWeightLabel: UILabel!
+    
+    var calculatorBrain = CalculatorBrain()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func heightSliderChanged(_ sender: UISlider) {
+        let currentHeight = String(format: "%.2f", sender.value)
+        currentHeightLabel.text = "\(currentHeight)m"
+    }
+    
+    @IBAction func weightSliderChanged(_ sender: UISlider) {
+        let currentWeight = Int(sender.value)
+        currentWeightLabel.text = "\(currentWeight)Kg"
+    }
+    
+    @IBAction func calculatePressed(_ sender: UIButton) {
+        let height = heightSlider.value
+        let weight = weightSlider.value
+        
+        calculatorBrain.calculateBMI( height: height, weight: weight)
+        self.performSegue(withIdentifier: "goToResult", sender: self)
+    
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "goToResult"{
+            let destinationVC = segue.destination as! ResultViewController
+            destinationVC.bmiValue = calculatorBrain.getBMIValue()
+            destinationVC.advice = calculatorBrain.getAdvice()
+            destinationVC.color = calculatorBrain.getColor()
+        }
+    }
+    
+    
+}
+
---
+++ BMI-Calculator/BMI Calculator/Models/BMI.swift
@@ -0,0 +1,16 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Münevver Elif Ay on 21.02.2023.
+//  Copyright © 2023 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+struct BMI{
+    let value: Float
+    let advice: String
+    let color: UIColor
+    
+}
